8af4b01538c8ba9ef5fe1af440a21fff9911fb81
Maschkov Konstantin <maschkovc@yandex.ru>
2024-01-15 13:41:59 +0300

    components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 
-import './App.scss';
+import './scss/app.scss';
+
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
+
 
 function App() {
     return (
-        <div className="App">
-            <h1>Hello pizza !</h1>
+        <div class="wrapper">
+            <Header />
+            <div class="content">
+                <div class="container">
+                    <div class="content__top">
+
+                        <Categories />
+                        <Sort />
+                    </div>
+                    <h2 class="content__title">Все пиццы</h2>
+                    <div class="content__items">
+                        <PizzaBlock title="Мексиканская" priсe="599" />
+                        <PizzaBlock title="Маргарита" />
+                        <PizzaBlock title="Пеперони" />
+                        <PizzaBlock />
+                        <PizzaBlock />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
