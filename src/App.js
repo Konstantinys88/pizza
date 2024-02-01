97fd7a2c615f75498a201d79d5ad12dc5ea91284
Maschkov Konstantin <maschkovc@yandex.ru>
2024-02-01 16:11:05 +0300

    router outlet

--- src/App.js
+++ src/App.js
@@ -1,38 +1,26 @@
 
 import './scss/app.scss';
 
-import Header from './components/Header';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import FullPizza from './pages/FullPizza';
 
 import { Routes, Route } from 'react-router';
 import Cart from './pages/Cart';
-
-
+import MainLayout from './layouts/MainLayout';
 
 function App() {
-
-
     return (
-        <div className="wrapper">
 
-            <Header />
-            <div className="content">
-                <div className="container">
-                    <Routes>
+        <Routes>
+            <Route path='/' element={<MainLayout />}>
+                <Route path='' element={<Home />} />
+                <Route path='cart' element={<Cart />} />
+                <Route path='*' element={<NotFound />} />
+                <Route path='pizza/:id' element={<FullPizza />} />
+            </Route>
+        </Routes>
 
-                        <Route path='/' element={<Home />} />
-                        <Route path='/cart' element={<Cart />} />
-                        <Route path='*' element={<NotFound />} />
-
-                        <Route path='/pizza/:id' element={<FullPizza />} />
-
-                    </Routes>
-                </div>
-            </div>
-
-        </div>
     );
 }
 
